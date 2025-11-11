08d314e104374fceb98a6cb4cd0296cc273c38bd
varnikapv <pvarnika05@gmail.com>
2025-11-11 10:46:56 +0530

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx'
 import  {useDebounce} from 'react-use'
+import { updateSearchCount } from './appwrite.js'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -45,7 +46,12 @@ if (data.Response === "False") {
   setMovieList([]);
   return;
 } 
+
+
+
 setMovieList(data.results || [] );
+
+updateSearchCount();
     } catch(error){
     console.error(`Error fetching movies: ${error}`);
     setErrorMessage('Failed to fetch movies. Please try again later.');
